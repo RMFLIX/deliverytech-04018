d9ad93bb23f181312bdffdb2df6419b6382fe7da
RMFLIX <https://github.com/RMFLIX/deliverytech-04018.git>
2026-05-11 11:57:07 -0300

    Dados atualizados.

--- src/main/java/com/deliveryth/delivery_api/config/CacheConfig.java
+++ src/main/java/com/deliveryth/delivery_api/config/CacheConfig.java
@@ -1,9 +1,23 @@
 package com.deliveryth.delivery_api.config;
 
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 @Configuration
 @EnableCaching
 public class CacheConfig {
+    
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory){
+       
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig();
+
+        return RedisCacheManager.builder(connectionFactory)
+        .cacheDefaults(config)
+        .build();
+    }
 }
